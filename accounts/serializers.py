8405e7c67a0145e5679f710856dd31479289adfe
dalienst <oduordalienst@gmail.com>
2025-01-07 13:29:28 +0300

    setup authentication

---
+++ accounts/serializers.py
@@ -0,0 +1,183 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+from accounts.validators import (
+    validate_password_digit,
+    validate_password_uppercase,
+    validate_password_lowercase,
+    validate_password_symbol,
+)
+from verification.models import VerificationCode
+from accounts.utils import send_verification_email
+
+User = get_user_model()
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token["id"] = str(user.id)
+        token["email"] = user.email
+        token["is_staff"] = user.is_staff
+        token["is_verified"] = user.is_verified
+        token["reference"] = user.reference
+        token["slug"] = user.slug
+
+        return token
+
+
+class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())],
+    )
+    password = serializers.CharField(
+        max_length=128,
+        min_length=5,
+        write_only=True,
+        validators=[
+            validate_password_digit,
+            validate_password_uppercase,
+            validate_password_symbol,
+            validate_password_lowercase,
+        ],
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "name",
+            "email",
+            "password",
+            "is_staff",
+            "is_superuser",
+            "is_verified",
+            "reference",
+            "slug",
+            "created_at",
+            "updated_at",
+        )
+
+    def create(self, validated_data):
+        # create user
+        user = User.objects.create_user(**validated_data)
+        user.save()
+
+        # create verification code
+        verification_code = VerificationCode.objects.create(
+            user=user, purpose="email_verification"
+        )
+
+        # send verification email
+        send_verification_email(user, verification_code.code)
+
+        return user
+
+
+class VerifyCodeSerializer(serializers.Serializer):
+    code = serializers.CharField()
+
+    def validate(self, attrs):
+        code = attrs.get("code")
+
+        try:
+            verification = VerificationCode.objects.get(
+                code=code, purpose="email_verification", used=False
+            )
+        except VerificationCode.DoesNotExist:
+            raise serializers.ValidationError("Invalid or expired verification code!")
+
+        try:
+            user = User.objects.get(email=verification.user.email)
+        except User.DoesNotExist:
+            raise serializers.ValidationError("Account with this email does not exist!")
+
+        try:
+            verification = user.verification_codes.get(
+                code=code, purpose="email_verification", used=False
+            )
+        except VerificationCode.DoesNotExist:
+            raise serializers.ValidationError("Invalid or expired verification code!")
+
+        if not verification.is_valid():
+            raise serializers.ValidationError(
+                "The code has expired or already been used!"
+            )
+
+        attrs["user"] = user
+        attrs["verification"] = verification
+
+        return attrs
+
+
+class RequestPasswordResetSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+
+    def validate_email(self, email):
+        if not User.objects.filter(email=email).exists():
+            raise serializers.ValidationError("Account with this email does not exist!")
+        return email
+
+    def save(self):
+        email = self.validated_data.get("email")
+        user = User.objects.get(email=email)
+
+        # create verification code
+        verification = VerificationCode.objects.create(
+            user=user, purpose="password_reset"
+        )
+
+        return verification
+
+
+class PasswordResetSerializer(serializers.Serializer):
+    code = serializers.CharField()
+    password = serializers.CharField(
+        max_length=128,
+        min_length=5,
+        write_only=True,
+        validators=[
+            validate_password_digit,
+            validate_password_uppercase,
+            validate_password_symbol,
+            validate_password_lowercase,
+        ],
+    )
+
+    def validate(self, attrs):
+        code = attrs.get("code")
+
+        try:
+            verification = VerificationCode.objects.get(
+                code=code, purpose="password_reset", used=False
+            )
+        except VerificationCode.DoesNotExist:
+            raise serializers.ValidationError("Invalid or expired verification code!")
+
+        if not verification.is_valid():
+            raise serializers.ValidationError(
+                "The code has expired or already been used!"
+            )
+
+        attrs["verification"] = verification
+        attrs["user"] = verification.user
+        return attrs
+
+    def save(self):
+        user = self.validated_data.get("user")
+        verification = self.validated_data.get("verification")
+        password = self.validated_data.get("password")
+
+        # update password
+        user.set_password(password)
+        user.save()
+
+        # mark code as used
+        verification.used = True
+        verification.save()
+
+        return user
---
+++ accounts/utils.py
@@ -0,0 +1,70 @@
+import string
+import secrets
+from datetime import datetime
+
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from monicasafarisapi.settings import EMAIL_USER
+
+
+current_year = datetime.now().year
+
+
+def generate_slug():
+    characters = string.ascii_letters + string.digits
+    random_string = "".join(secrets.choice(characters) for _ in range(16))
+    return random_string.upper()
+
+
+def generate_reference():
+    characters = string.ascii_letters + string.digits
+    random_string = "".join(secrets.choice(characters) for _ in range(10))
+    return random_string.upper()
+
+
+def send_verification_email(user, verification_code):
+    """
+    A function to send a verification email
+    """
+    current_year = datetime.now().year
+    email_body = render_to_string(
+        "account_verification.html",
+        {
+            "user": user,
+            "verification_code": verification_code,
+            "current_year": current_year,
+        },
+    )
+
+    send_mail(
+        subject="Verify your account",
+        message="",
+        from_email=EMAIL_USER,
+        recipient_list=[user.email],
+        fail_silently=False,
+        html_message=email_body,
+    )
+
+
+def send_password_reset_email(user, verification_code):
+    """
+    A function to send a password reset email
+    """
+    current_year = datetime.now().year
+    email_body = render_to_string(
+        "password_reset.html",
+        {
+            "user": user,
+            "verification_code": verification_code,
+            "current_year": current_year,
+        },
+    )
+
+    send_mail(
+        subject="Reset your password",
+        message="",
+        from_email=EMAIL_USER,
+        recipient_list=[user.email],
+        fail_silently=False,
+        html_message=email_body,
+    )
