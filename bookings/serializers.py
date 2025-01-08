7265e05ba93d72e323a7f5ff9d2a1eea67a0dcc4
dalienst <oduordalienst@gmail.com>
2025-01-08 23:54:21 +0300

    add serializers

--- bookings/serializers.py
+++ bookings/serializers.py
@@ -8,6 +8,31 @@ User = get_user_model()
 
 
 class BookingSerializer(serializers.ModelSerializer):
-    tours = serializers.SlugRelatedField(
+    tour = serializers.SlugRelatedField(
         queryset=Tour.objects.all(), slug_field="reference"
     )
+    name = serializers.CharField(max_length=255)
+    email = serializers.EmailField()
+
+    class Meta:
+        model = Booking
+        fields = (
+            "id",
+            "tour",
+            "name",
+            "email",
+            "phone",
+            "date",
+            "guests",
+            "special_requests",
+            "status",
+            "request_jeep",
+            "currency",
+            "amount",
+            "payment_method",
+            "payment_status",
+            "created_at",
+            "updated_at",
+            "slug",
+            "reference",
+        )
